97278f77629632566139c4c6b6af148789ce114f
beatrixxxxxte <beatrizdeoliveira45@icloud.com>
2024-06-23 17:34:39 -0300

    Delete codigo/frontend directory

--- codigo/frontend/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import './index.css'; // Certifique-se de importar o CSS aqui
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
